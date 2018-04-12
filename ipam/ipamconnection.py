bea24670e7f5145657b3c12bd5b3f8ff9e7eb181
Maurice Green <bernard.infosystems@gmail.com>
2018-04-12 14:07:13 -0500

    Clean up and correction
    Signed-off-by: Maurice <mgre2324@gmail.com>

--- ipam/ipamconnection.py
+++ ipam/ipamconnection.py
@@ -1,67 +1,73 @@
+import os
+import pycurl
+import StringIO
+import json
+import getpass
+from urllib import urlencode
 
 
 class UrlMeta(type):
     _instances = {}
 
     def __call__(cls, *args, **kwargs):
-        if cls not in _instances:
-            _instances[cls] = super(UrlMeta, cls).__call__(*args, **kwargs)
-        return _instances[cls]
+        if cls not in cls._instances:
+            cls._instances[cls] = super(UrlMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
 
 class IpamConnection(object):
 
     __metaclass__ = UrlMeta
-
 
     def __init__(self, sheaders=None):
         
         self.headers = {
             'content-Length:': '53'
         }
-
+        
         self.user = os.environ['USER']
-        print "Please Enter Your AD Password for: %s\n" % self.user
-        self.passwd = getpass.getpass()
+        self.passwd = getpass.getpass("Enter Password For \({}\)".format(self.user))
         self.curl = pycurl.Curl()
-        opts = {
-            self.curl.POST: 1,
-            self.curl.CONNECTTIMEOUT: 60,
-            self.curl.TIMEOUT: 15,
-            self.curl.SSL_VERIFYPEER: 0,
-            self.curl.SSL_VERIFYHOST: 0
+        
+        self._opts = {
+	        "self.curl.POST": 1,
+	        "self.curl.CONNECTTIMEOUT": 60,
+	        "self.curl.TIMEOUT": 15,
+	        "self.curl.SSL_VERIFYPEER": 0,
+	        "self.curl.SSL_VERIFYHOST": 0
         }
 
-        for k, v in opts:
+        for k, v in self._opts.items():
             self.curl.setopt(k, v)
-
-        for k, v in headers:
-            self.curl.setopt(self.curl.HTTPHEADER, ["".join(k, v)])
+        
+        self.curl.setopt(pycurl.HTTPHEADER, ["".join([k, v]) for k, v in self.headers.items()])
         
         if self.user is not None:
-            self.curl.setopt(self.curl.USERPWD, '%s:%s' % (self.user, self.passwd))
+            self.curl.setopt(pycurl.USERPWD, '%s:%s' % (self.user, self.passwd))
+
 
 class ConnectionAction(IpamConnection):
 
     def __init__(self, sheaders=None):
         super(ConnectionAction, self).__init__()
-
+        
         if sheaders is not None:
             self.headers.update(sheaders)
 
     def gather(self, *args, **kwargs):
-        responseBuffer = StringIO.StringIO()
+        responsebuffer = StringIO.StringIO()
 
         if not all(i in kwargs for i in args):
-            raise('')
+            raise ValueError('Missing Arguments in KWARGS\n')
 
-        if 'postfields' in kwargs:
-            params = urlencode(kwargs['postfields'])
-            self.curl.setopt(self.curl.POSTFIELDS, params)
+        if 'POSTFIELDS' in kwargs:
+            params = urlencode(kwargs['POSTFIELDS'])
+            self.curl.setopt(pycurl.POSTFIELDS, params)
 
         for i in args:
-            self.curl.setopt(self.curl.i, kwargs[i])
+            self.curl.setopt(pycurl.i, kwargs[i])
 
         self.curl.perform()
-        responseBuffer.seek(0)
+        responsebuffer.seek(0)
 
-        return json.loads(responseBuffer.readlines()[0])[kwargs['data']][kwargs['set']]
+        return json.loads(responsebuffer.readlines()[0])[kwargs['data']][kwargs['set']]
