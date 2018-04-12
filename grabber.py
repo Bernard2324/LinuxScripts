ac0284df6df80830ab881cebbc06bdb89708d3d5
Maurice <mgre2324@gmail.com>
2018-04-11 23:49:23 -0500

    cleanup

--- grabber.py
+++ grabber.py
@@ -6,18 +6,31 @@ Usage:  IPAM CLI
 '''
 
 from ipam_main_class import url_obj
+from collections import abc
+
+
+
+state = True
+
+class SubnetFeed(object):
+
+    def __init__(self, fcd, select, param):
+
+        if not isinstance(self._obj, abc.MutableSequence):
+            raise StandardError("")
+        self._obj = fcd
+
+        SubnetFeed.getid(self._obj, select, param)
+    
+    @classmethod
+    def getparam(cls, obj, select, pin, param):
+        for subnet in self._obj['data']:
+            if select is None:
+                return subnet[param]
+            if subnet[pin] == select:
+                return subnet[param]
 
 class Grabber(url_obj):
-
-    def __init__(self):
-        url_obj.__init__(self)
-
-    def get_token(self):
-        fields = {'ipamusername': self.user, 'ipampassword': self.passwd}
-        token_grab = 'https://linkto.server.local/api/%s/user/' % self.user
-        datablock = url_obj.quick_build(token_grab, fields)
-        ipam_token = datablock['data']['token']
-        return ipam_token
 
     def get_sections(self, token, section_choice=""):
         headers = [
@@ -26,67 +39,66 @@ class Grabber(url_obj):
             'Token: %s' % str(token)
         ]
         url = "https://linkto.server.local/api/johndoe/sections/"
-        datablock = url_obj.quick_build(url, token_val=token, postfields=[])
-        if section_choice == "ALL":
-            company_dcenters = [section['name'] for section in datablock['data']]
-            datacenter_ids = [section['id'] for section in datablock['data']]
-            name_constructors = [(str(server_type)+dc) for dc in company_dcenters]
-            return (company_dcenters, datacenter_ids, name_constructors)
-        else:
-            target_id = [section['id'] for section in datablock['data'] if section['name'] == section_choice]
-            return str(target_id[0])
+        datablock = url_obj.quick_build(url)
+        if section_choice != "ALL":
+            return SubnetFeed(datablock, section_choice, 'name', 'id')[0]
+        
+        company_dcenters = SubnetFeed(datablock, None, 'name')
+        datacenter_ids = SubnetFeed(datablock, None, 'id')
+
+        name_constructors = [
+            (str(server_type)+dc) for dc in company_dcenters
+        ]
+        return (company_dcenters, datacenter_ids, name_constructors)
 
     def get_subnets(self, token, dc_id):
-        ipam_token = token
-        orig_URI = "https://linkto.server.local/api/%s/sections/%s/subnets/" % (self.user, dc_id)
-        first_call_data = url_obj.quick_build(orig_URI, token_val=ipam_token, postfields=[])
+        orig_URI = "https://linkto.server.local/api/{}/sections/{}/subnets/".format(
+            self.user, dc_id
+        )
+        first_call_data = url_obj.quick_build(orig_URI)
+
         # Use the nested function above for easier recursion
         def decision(sub_selection):
             selection = sub_selection.strip()
-            sec_sub_id = [subnet['id'] for subnet in first_call_data['data'] if subnet['subnet'] == selection]
-            id_holder = str(sec_sub_id[0])
-            new_uri = "https://linkto.server.local/api/%s/subnets/%s/first_free/" % (self.user, id_holder)
-            second_call_data = url_obj.quick_build(new_uri, token_val=ipam_token, postfields=[])
+            id_holder = SubnetFeed(first_call_data, 'subnet', selection, 'id')[0]
+
+            new_uri = "https://linkto.server.local/api/{}/subnets/{}/first_free/".format(
+                self.user, id_holder
+            )
+
+            second_call_data = url_obj.quick_build(new_uri)
+
             # return (id_holder, second_call_data['data'])
-            print "First Free IP Address For Selected Subet: %s" % second_call_data['data']
+            return second_call_data['data']
+            
             url_obj.close()
-        if sys.argv[3] == "--list":
-            # print list of subnets and desctriptions
-            desclist = [(subnet['subnet'], subnet['mask'], subnet['description']) for subnet in first_call_data['data']]
-            print "\t\t ------------ Subnet/Mask ---------- \t Description -------------\n"
-            state = True
-            while state:
-                divider = len(desclist)/3
-                for level in desclist[:divider]:
-                    (subnet, mask, desc) = level
-                    print "\t\t %s/%s -------- %s" % (subnet, mask, desc)
-                selection = raw_input("Enter Subnet Selection (type 'continue' to print more):  ")
-                selection = selection.strip()
-                if selection == "continue":
-                    for level in desclist[divider:divider*2]:
-                        (subnet, mask, desc) = level
-                        print "\t\t %s/%s -------- %s" % (subnet, mask, desc)
-                    selection = raw_input("Enter Subnet Selection (type 'continue' to print more):  ")
-                    selection = selection.strip()
-                    if selection == "continue":
-                        for level in desclist[divider*2:]:
-                            (subnet, mask, desc) = level
-                            print "\t\t %s/%s -------- %s" % (subnet, mask, desc)
-                        selection = raw_input("Enter Subnet Selection (Type: 'Ctrl + C' To Cancel):  ")
-                        selection = selection.strip()
-                        decision(selection)
-                        state = False
-                    else:
-                        decision(selection)
-                        state = False
-                else:
-                    decision(selection)
-                    state = False
-        else:
+        
+        if not sys.argv[3] == "--list":
+            sec_sub_id = SubnetFeed(first_call_data, sys.argv[3], 'id')
             # print specific.  Just in case the subnet desired is already known
-            sec_sub_id = [subnet['id'] for subnet in first_call_data['data'] if subnet['subnet'] == str(sys.argv[3])]
-            id_holder = str(sec_sub_id[0])
-            new_uri = "https://linkto.server.local/api/%s/subnets/%s/first_free/" % (self.user, id_holder)
+
+            id_holder = sec_sub_id
+
+            new_uri = "https://linkto.server.local/api/{}/subnets/{}/first_free/".format(
+                self.user, id_holder
+            )
+
             second_call_data = url_obj.quick_build(new_uri, token_val=ipam_token, postfields=[])
             return (id_holder, second_call_data['data'])
             url_obj.close()
+
+
+        selection = raw_input("Enter Subnet Selection (type 'continue' to print more):  ").strip()
+        desclist = [
+            (subnet['subnet'], subnet['mask'], subnet['description']) for subnet in first_call_data['data']
+        ]
+
+        while state:
+            divider = len(desclist)/3
+
+            while selection == "continue":
+                (s, m, d) = [level for level in desclist[:divider:divider*2]]
+                print (s, m, d)
+                selection = raw_input("Enter Subnet Selection (type 'continue' to print more): ").strip()
+
+            decision(selection)
---
+++ ipamconnection.py
@@ -0,0 +1,67 @@
+
+
+class UrlMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in _instances:
+            _instances[cls] = super(UrlMeta, cls).__call__(*args, **kwargs)
+        return _instances[cls]
+
+class IpamConnection(object):
+
+    __metaclass__ = UrlMeta
+
+
+    def __init__(self, sheaders=None):
+        
+        self.headers = {
+            'content-Length:': '53'
+        }
+
+        self.user = os.environ['USER']
+        print "Please Enter Your AD Password for: %s\n" % self.user
+        self.passwd = getpass.getpass()
+        self.curl = pycurl.Curl()
+        opts = {
+            self.curl.POST: 1,
+            self.curl.CONNECTTIMEOUT: 60,
+            self.curl.TIMEOUT: 15,
+            self.curl.SSL_VERIFYPEER: 0,
+            self.curl.SSL_VERIFYHOST: 0
+        }
+
+        for k, v in opts:
+            self.curl.setopt(k, v)
+
+        for k, v in headers:
+            self.curl.setopt(self.curl.HTTPHEADER, ["".join(k, v)])
+        
+        if self.user is not None:
+            self.curl.setopt(self.curl.USERPWD, '%s:%s' % (self.user, self.passwd))
+
+class ConnectionAction(IpamConnection):
+
+    def __init__(self, sheaders=None):
+        super(ConnectionAction, self).__init__()
+
+        if sheaders is not None:
+            self.header.update(sheaders)
+
+    def gather(self, *args, **kwargs):
+        responseBuffer = StringIO.StringIO()
+
+        if not all(i in kwargs for i in args):
+            raise('')
+
+        if 'postfields' in kwargs:
+            params = urlencode(kwargs['postfields'])
+            self.curl.setopt(self.curl.POSTFIELDS, params)
+
+        for i in args:
+            self.curl.setopt(self.curl.i, kwargs[i])
+
+        self.curl.perform()
+        responseBuffer.seek(0)
+
+        return json.loads(responseBuffer.readlines()[0])[kwargs['data']][kwargs['set']]
